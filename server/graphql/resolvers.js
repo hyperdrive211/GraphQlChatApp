2e8ba3c4c48e3ba46b11ad6c5b106f6af8a9109e
Johnny <jmccauley117@gmail.com>
2021-04-20 16:31:38 +0100

    using database contraints to return validation messages with sequelize

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,4 +1,6 @@
 const {user} = require('../models'); 
+const bcrypt = require('bcryptjs'); 
+const {UserInputError} = require('apollo-server'); 
 
 module.exports = {
     Query: {
@@ -14,19 +16,33 @@ module.exports = {
     Mutation: {
         register: async (_, args) => {
             const {username, email, password, confirmPassword} = args; 
+            let errors = {}; 
             try{
                 //validate data 
-
+                if(email.trim() === '') errors.email = "Email must not be empty"; 
+                if(username.trim() === '') errors.username = "Username must not be empty"; 
+                if(password.trim() === '') errors.password = "Password must not be empty"; 
+                if(confirmPassword.trim() === '') errors.password = "Confirm Password must not be empty"; 
+                if(password !== confirmPassword) errors.password = "Passwords do not match"; 
                 //check that username and email already exist
-
-                //check that passwords match
+                if(Object.keys(errors).length > 0){
+                      throw new UserInputError("Incorrect input", {errors}); 
+                }
+                //hash the password
+                hashedPassword = await bcrypt.hash(password, 6); 
 
                 //create User
-
-                //return User
-
+                const newUser = await user.create({
+                    username, email, password: hashedPassword, 
+                })
+                
+                //return new User
+                return newUser; 
             }catch(err){
-                throw new Error(err); 
+                if(err.name === 'SequelizeUniqueConstraintError'){
+                     err.errors.forEach(e => {errors[e.path] = e.message}); 
+                }
+                throw new UserInputError('Bad Input', {errors}); 
             }
         }
     }
